cfb8cc8f99f36328893701b9e076a60f9e53c8ce
Alon Itach <alon.itach@mail.huji.ac.il>
2019-06-07 16:50:45 +0300

    q3

--- analysis.py
+++ analysis.py
@@ -19,39 +19,34 @@ def question2():
   return answerDiscount, answerNoise
 
 def question3a():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.1
+  answerNoise = 0.01
+  answerLivingReward = -3
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = 0.9
-  answerNoise = 0.2
+  answerDiscount = 0.1
+  answerNoise = 0.1
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 1
+  answerNoise = 0.001
+  answerLivingReward = -2
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 1
+  answerNoise = 0.3
+  answerLivingReward = -1
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.1
+  answerNoise = 0.5
+  answerLivingReward = 100
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question6():
   answerEpsilon = None
